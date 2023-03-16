d245e989b847ebf1fd78318502fa692f57522204
pallab nath <you@example.com>
2023-03-16 18:55:18 +0530

    Pizza made

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,19 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean cheese ;
+    private boolean toppings ;
+    private boolean take ;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg==true)
+        {
+            price = price + 300 ;
+        }
+        else {
+            price = price + 400 ;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +26,66 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+
+        if(!cheese)
+        {
+            cheese = true ;
+            price = price + 80 ;
+        }
+
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!toppings) {
+            toppings = true;
+            if (isVeg == true) {
+                price = price + 70;
+            } else {
+                price = price + 120;
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!take) {
+            price = price + 20;
+            take = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        if(isVeg==true)
+        {
+            this.bill = "Base Price Of The Pizza: " + 300;
+        }
+        else {
+            this.bill = "Base Price Of The Pizza: " + 400;
+        }
+
+        if(this.cheese)
+        {
+            this.bill=this.bill+ "\n" +"Extra Cheese Added: "+80;
+        }
+
+        if(this.toppings)
+        {
+            if(this.isVeg)
+            {
+                this.bill=this.bill+"\n" +"Extra Toppings Added: "+70;
+            }
+            else
+            {
+                this.bill=this.bill+"\n" +"Extra Toppings Added: "+120;
+            }
+        }
+
+        if(take==true)
+        {
+            this.bill=this.bill+'\n'+"Paperbag Added: "+20;
+        }
+
+        this.bill=this.bill+"\n"+"Total Price: "+this.price+"\n" ;
+
+            return this.bill;
     }
 }
